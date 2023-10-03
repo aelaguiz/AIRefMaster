de4baae76bc72f756853808e8014ca2ef1bbdfa0
Amir Elaguizy <aelaguiz@gmail.com>
2023-10-03 16:46:31 -0500

    WIP

--- main.py
+++ main.py
@@ -2,52 +2,64 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
-
 import os.path
 import pickle
+import json
 
-# If modifying these SCOPES, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
-def get_credentials():
+def load_config():
+    with open("config.json", "r") as f:
+        return json.load(f)
 
+def get_credentials(account_name, config):
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    credentials_file = config[account_name]["credentials_file"]
+    token_file = f"{account_name}_token.pickle"
+
+    if os.path.exists(token_file):
+        with open(token_file, 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, prompt the user to log in.
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(token_file, 'wb') as token:
             pickle.dump(creds, token)
 
     return creds
 
-def list_files(service):
-    """List all files in Google Drive"""
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(f"{item['name']} ({item['id']})")
-
+def list_accounts(config):
+    for account in config.keys():
+        token_file = f"{account}_token.pickle"
+        status = "Authenticated" if os.path.exists(token_file) else "Not Authenticated"
+        print(f"{account} - {status}")
 
 if __name__ == '__main__':
-    creds = get_credentials()
+    config = load_config()
 
-    service = build('drive', 'v3', credentials=creds)
+    while True:
+        print("\nOptions:")
+        print("1 - List Google accounts and OAuth status")
+        print("2 - Authenticate to a Google account")
+        print("3 - Exit")
 
-    list_files(service)
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            list_accounts(config)
+        elif choice == "2":
+            print("Available accounts:")
+            for account in config.keys():
+                print(account)
+            account_name = input("Enter the name of the account to authenticate: ")
+            if account_name in config:
+                get_credentials(account_name, config)
+                print(f"Authenticated to {account_name}")
+            else:
+                print("Invalid account name")
+        elif choice == "3":
+            break
